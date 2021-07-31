741d5ef8bc725d892e023580c6070fbb881a60c0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-07-31 12:34:30 +0900

    add jannson-test

---
+++ recipes-jansson-test/jansson-test/jansson-test_0.1.bb
@@ -0,0 +1,18 @@
+SUMMARY = "bitbake-layers recipe"
+DESCRIPTION = "Recipe created by bitbake-layers"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${THISDIR}/COPYING;md5=d41d8cd98f00b204e9800998ecf8427e"
+
+FILESEXTRAPATHS_prepend := "${THISDIR}:"
+
+SRC_URI = "file://src/*"
+
+S = "${WORKDIR}/src"
+
+inherit autotools
+
+DEPENDS = "jansson"
+
+
+
+RDEPENDS_${PN} = "jansson"
---
+++ recipes-jansson-test/jansson-test/src/testoverwrite.c
@@ -0,0 +1,49 @@
+#include <string.h>
+#include <jansson.h>
+
+int main(int argc, char *argv[])
+{
+    json_t* json_root_object;
+    json_error_t json_error;
+
+    if (argc == 2)
+    {
+        json_root_object = json_load_file(argv[1],0,&json_error);
+    }
+    else
+    {
+        json_root_object = json_object();
+    }
+
+    json_object_set_new(json_root_object, "test1", json_integer(42));
+    json_object_set_new(json_root_object, "test2", json_integer(44));
+
+    json_t* json_int = json_object_get(json_root_object, "test1");
+    if (json_int)
+    {
+        json_integer_set(json_int, 10000);
+    }
+
+    {
+        json_t *obj = json_object();
+        json_object_set_new(obj, "test1", json_integer(100));
+        json_object_set_new(obj, "test2", json_integer(200));
+        json_object_set_new(json_root_object, "hash2", obj);
+    }
+
+    {
+        json_t *obj = json_array();
+        json_array_append_new(obj, json_string("ABCDE"));
+        json_array_append_new(obj, json_string("12345"));
+        json_array_append_new(obj, json_integer(200));
+        json_object_set_new(json_root_object, "arraytest", obj);
+    }
+
+    char * dump = json_dumps(json_root_object, JSON_INDENT(4));
+    if (dump)
+    {
+        printf("%s\n", dump);
+        free(dump);
+    }
+    json_decref(json_root_object);
+}
