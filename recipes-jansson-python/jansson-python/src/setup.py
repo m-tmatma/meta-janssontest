c699eb7f5963fa26195551395081601de080c528
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-08-01 15:14:10 +0900

    add sub module

--- recipes-jansson-python/jansson-python/src/setup.py
+++ recipes-jansson-python/jansson-python/src/setup.py
@@ -4,5 +4,8 @@ setup(
     name='janssonbind',
     version="0.1.0",
     install_requires=[],
-    packages=['janssonbind']
+    packages=[
+        'janssonbind',
+        'janssonbind.util'
+    ]
 )
